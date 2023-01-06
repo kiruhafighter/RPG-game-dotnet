a1d0acf91969eeafff6483801f08ea3a203031d5
Kyrylo Salomatin <kirill.salomatin@itperspectives.ro>
2023-01-06 11:45:00 +0200

    Add dependency injection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+global using RPG_game_dotnet.Models;
+using RPG_game_dotnet.Services.CharacterService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
